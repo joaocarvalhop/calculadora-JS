339f9960d511ed294dacff5dd5144c4eb3663a98
joaocarvalhop <86267920+joaocarvalhop@users.noreply.github.com>
2022-02-02 21:02:03 -0300

    Eval - Validando o primeiro operador e calculando resultado

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -41,35 +41,50 @@ class CalcController {
   }
 
   isOperator(value) {
-    return (["+", "-", "*", "/", "%"].indexOf(value) > -1);
+    return ["+", "-", "*", "/", "%"].indexOf(value) > -1;
+  }
+
+  calc() {
+    let last = this._operation.pop();
+
+    let result = eval(this._operation.join(""));
+
+    // novo array
+    this._operation = [result, last];
+  }
+
+  pushOperator(value) {
+    this._operation.push(value);
+
+    if (this._operation.length > 3) {
+      this.calc();
+    }
+  }
+
+  setLastNumberOnDisplay() {
+
+    
   }
 
   addOperation(value) {
-
-    console.log("A", isNaN(this.getLastOperation()));
-
     if (isNaN(this.getLastOperation())) {
-
       if (this.isOperator(value)) {
-
         this.setLastOperation(value);
-
       } else if (isNaN(value)) {
-
-        console.log(value);
-
+        console.log("Outra coisa", value);
       } else {
-
-        this._operation.push(value);
-
+        this.pushOperator(value);
       }
     } else {
+      if (this.isOperator(value)) {
+        this.pushOperator(value);
+      } else {
+        let newValue = this.getLastOperation().toString() + value.toString();
+        this.setLastOperation(parseInt(newValue));
 
-      let newValue = this.getLastOperation().toString() + value.toString();
-      this.setLastOperation(parseInt(newValue));
+        this.setLastNumberOnDisplay();
+      }
     }
-
-    console.log(this._operation);
   }
 
   setError() {
